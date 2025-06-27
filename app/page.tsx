33b225939765a4cb6a475ec12d208e272f07ad43
Li Kanhang <664974881@qq.com>
2025-06-27 04:02:16 -0400

    fix

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { BASEURL, MODEL } from'./constants/bots';
+import loader from '../assets/loading.webp';
+import ChatBot from './components/chatbot';
+
+export default function ServerSide() {
+
+
+
+  return (
+    <div>
+      <ChatBot/>
+    </div>
+  );
+}
